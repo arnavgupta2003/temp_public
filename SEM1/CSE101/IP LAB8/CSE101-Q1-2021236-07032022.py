d17a5d1d1946ffb4d6f30c73d54acff3b9aa4518
Arnav Gupta <arnav.gupta.2003@gmail.com>
2023-12-10 15:42:05 +0530

    Only Sem 2 RES

--- SEM1/CSE101/IP LAB8/CSE101-Q1-2021236-07032022.py
+++
@@ -1,8 +0,0 @@
-def countNum(l):
-	if not l:
-		return 0
-	else:
-		return 1+countNum(l[1:])
-
-lst=[x for x in input().split()]
-print(countNum(lst))			
--- SEM1/CSE101/IP LAB8/CSE101-Q3-2021236-07032022.py
+++
@@ -1,9 +0,0 @@
-def fn(n,x,y,t,u,v):
-	l=abs(x-u)
-	d=abs(y-v)
-	if(l+d<=t):
-		print("Yes")
-	else:
-		print("No")				
-fn()		
-#Not working
